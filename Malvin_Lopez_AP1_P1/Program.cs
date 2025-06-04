14c0cba2e6eb086f3c53c64d0da5b6993d4c738f
Malvin <Malvinjose16@gmail.com>
2025-06-03 22:24:41 -0400

    Crea RegistroIndex

--- Malvin_Lopez_AP1_P1/Program.cs
+++ Malvin_Lopez_AP1_P1/Program.cs
@@ -1,3 +1,4 @@
+using Blazored.Toast;
 using Malvin_Lopez_AP1_P1.Components;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,6 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+builder.Services.AddBlazoredToast();
+
 
 var app = builder.Build();
 
