64b88655a0dc51edbf2106294ca621ba73a28b90
Malvin <Malvinjose16@gmail.com>
2025-06-03 21:29:34 -0400

    Crea Carpetas y componentes necesarios

---
+++ Malvin_Lopez_AP1_P1/Dal/Contexto.cs
@@ -0,0 +1,6 @@
+﻿namespace Malvin_Lopez_AP1_P1.Dal
+{
+    public class Contexto
+    {
+    }
+}
---
+++ Malvin_Lopez_AP1_P1/Services/RegistroService.cs
@@ -0,0 +1,6 @@
+﻿namespace Malvin_Lopez_AP1_P1.Services
+{
+    public class RegistroService
+    {
+    }
+}
