bdca5f9dd0797194f1be8461f6ab544e55737c55
macbookair <macbookair@MACBOOKAIRDCC0>
2025-06-09 20:25:21 -0400

    modfica contexto

--- Malvin_Lopez_AP1_P1/Dal/Contexto.cs
+++ Malvin_Lopez_AP1_P1/Dal/Contexto.cs
@@ -1,4 +1,16 @@
-﻿namespace Malvin_Lopez_AP1_P1.Dal
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+
+namespace Malvin_Lopez_AP1_P1.Dal
 {
-    
+    public class Contexto: DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+        }
+
+        public DbSet<Models.RegistroAportes> Registros { get; set; } = null!;
+
+        public Contexto() { }
+    }
 }
