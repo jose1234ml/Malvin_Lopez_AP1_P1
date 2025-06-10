acfdc41f643e1fe7bb01b27fb34561989618d48e
macbookair <macbookair@MACBOOKAIRDCC0>
2025-06-09 20:26:21 -0400

    Crea registro aportes

---
+++ Malvin_Lopez_AP1_P1/Models/RegistroAportes.cs
@@ -0,0 +1,23 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace Malvin_Lopez_AP1_P1.Models;
+
+public class RegistroAportes
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "El nombre es obligatorio")]
+    [StringLength(100, ErrorMessage = "El nombre no puede tener más de 100 caracteres")]
+    [RegularExpression(@"^[^\d]*$", ErrorMessage = "El nombre no puede contener números")]
+    public string Nombre { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "La fecha es obligatoria")]
+    [DataType(DataType.Date)]
+    public DateTime Fecha { get; set; } = DateTime.Today;
+
+    [Required(ErrorMessage = "Debe ingresar el monto aportado.")]
+    [Range(0.01, double.MaxValue, ErrorMessage = "El dinero aportado debe ser mayor a 0")]
+    public decimal DineroAportes { get; set; }
+}
